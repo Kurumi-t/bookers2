05a39eee7c4c689a722cb4fc139b4e58e67ba517
EC2 Default User <ec2-user@ip-172-31-15-230.ec2.internal>
2022-05-29 00:33:33 +0000

    [Add]課題３bookのindexページ左側New book作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
-  def new
-  end
+  
 
   def index
   end
@@ -10,4 +9,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:shop_name, :image, :caption)
+  end
+  
 end
