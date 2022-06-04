69d705ca52da63fc8be98205d3c019f16f5d6961
EC2 Default User <ec2-user@ip-172-31-15-230.ec2.internal>
2022-06-04 05:07:13 +0000

    [Add]userのupdate機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(user.id)
   end
   
   private
