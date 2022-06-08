409da0ca2ba7c16dbf91f74df91d56244c12a456
EC2 Default User <ec2-user@ip-172-31-15-230.ec2.internal>
2022-06-08 09:39:46 +0000

    [Add]バリデーションの実装（投稿の更新が失敗した時）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,13 +33,17 @@ class BooksController < ApplicationController
   end
   
   def edit
-   @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: 'You have updated book successfully.'
+    @book = book
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
   
   private
